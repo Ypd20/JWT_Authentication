98ee2254e35a4b12eb134d155ec3c13f1a53575a
Ypd20 <101164198+Ypd20@users.noreply.github.com>
2022-12-20 11:43:03 +0530

    Update auth.py

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -3,7 +3,7 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from ..routes.tokens import decodeJWT
 
-"""Verifying the protected roues using JWTBearer"""
+"""Verifying the protected routes using JWTBearer"""
 class JWTBearer(HTTPBearer):
     def __init__(self, auto_error: bool = True):
         super(JWTBearer, self).__init__(auto_error=auto_error)
